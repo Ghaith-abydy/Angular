2d80a9e63005debdeea3d1c8dbfeae311fa57072
Ghaith-abydy <ghaith2020abidi@gmail.com>
2023-10-17 22:31:11 +0200

    Tp6

---
+++ mon_projet/src/app/produits/produits.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+})
+export class ProduitsComponent {
+
+
+  produits : Produit[];
+
+  constructor(private produitService: ProduitService ) {
+
+    this.produits = produitService.listeProduits();
+  }
+
+  supprimerProduit(p: Produit)
+  {
+    //console.log(p);
+    let conf = confirm("Etes-vous sûr ?");
+
+    if (conf)
+       this.produitService.supprimerProduit(p);
+  }
+}
